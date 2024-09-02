6c661d2cb586cb5d63840a2e7593f071b6cce84e
Felics666 <felics6@gmail.com>
2024-09-02 18:24:33 +0300

    Initial commit

--- src/main/java/pro/sky/skyprocalculatorspring/SkyproCalculatorSpringApplication.java
+++ src/main/java/pro/sky/skyprocalculatorspring/SkyproCalculatorSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SkyproCalculatorSpringApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SkyproCalculatorSpringApplication.class, args);
 	}
 
