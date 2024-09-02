fcd6324ed367a0375c6d7a3051260695d4049ba3
Felics666 <felics6@gmail.com>
2024-09-02 18:19:26 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyprocalculatorspring/SkyproCalculatorSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprocalculatorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCalculatorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCalculatorSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprocalculatorspring/SkyproCalculatorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprocalculatorspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproCalculatorSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
