bbcd2eec26f53562f16a56bb5c60830b376c1a25
Felics666 <felics6@gmail.com>
2024-09-08 15:46:22 +0300

    Added task.

--- src/main/java/pro/sky/skyprocalculatorspring/SkyproCalculatorSpringApplication.java
+++ src/main/java/pro/sky/skyprocalculatorspring/SkyproCalculatorSpringApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SkyproCalculatorSpringApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(SkyproCalculatorSpringApplication.class, args);
-	}
+        SpringApplication.run(SkyproCalculatorSpringApplication.class, args);
+    }
 
 }
